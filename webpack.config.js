122d07273f6cc61b43e5eddc09f1b1cf29c0968a
Travis Hubbard <t3patterson@gmail.com>
2017-02-28 13:44:24 -0500

    updating babel preset

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,12 @@ module.exports = {
   output : { filename: './dist/js/app.js' },
   module: {
     loaders: [
-		 {
+		 { 
           test: /\.js$/,
 			 exclude: /node_modules/,
 			 loader: 'babel-loader',
           query: {
-             presets: ['es2015']
+             presets: ['es2015', 'react']
           }
 		 },
 		 {
